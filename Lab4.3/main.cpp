2908a364d6eaf6f825838643ec7237b17e4886ad
GGrishaa <145452868+GGrishaa@users.noreply.github.com>
2023-10-12 02:57:05 +0300

    Initial Commit

---
+++ Lab4.3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab4.3
+//
+//  Created by Григорий Миронов on 12.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
